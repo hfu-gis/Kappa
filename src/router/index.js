3250e2f382cc8f5c0357ac5798ec6da8103928d9
daniel.apfel@hs-furtwangen.de <fxcbykz9655>
2019-11-20 11:53:26 +0100

    abo erstellen und daten bearbeiten

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login_vue'
 import HomePage from '../views/Home'
+import Abo_anlegen from '../views/Abo_anlegen'
+import abobearbeiten from "../views/abobearbeiten";
+
 import HelloWorld from "../components/HelloWorld";
 
 Vue.use(VueRouter)
@@ -15,6 +18,14 @@ export default new VueRouter({
         {
             path: '/login',
             component: Login
+        },
+        {
+            path: '/Abo_anlegen',
+            component: Abo_anlegen
+        },
+        {
+            path: '/abobearbeiten',
+            component: abobearbeiten
         },
     ]
 })
