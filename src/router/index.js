35e5d6f88be16969baabe2f989933d2ba11765d2
RedouAin <56726424+RedouAin@users.noreply.github.com>
2019-11-20 12:14:44 +0100

    login vue passwortfalsch vue

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import HomePage from '../views/Home'
 import Abo_anlegen from '../views/Abo_anlegen'
 import abobearbeiten from "../views/abobearbeiten";
 import Passwortvergessen from "../views/Passwortvergessen";
+import Passwortfalsch from "../views/Passwortfalsch";
+import Registrieren from "../views/Registrieren";
 
 import HelloWorld from "../components/HelloWorld";
 
@@ -31,6 +33,14 @@ export default new VueRouter({
         {
             path: '/Passwortvergessen',
             component: Passwortvergessen
+        }
+        {
+            path: '/Passwortfalsch'
+            component: Passwortfalsch
+        }
+        {
+            path: '/Registrieren'
+            component: Registrieren
         }
     ]
 })
