8136cf44755dafcc938224f06eb2283d70e69291
daniel.apfel@hs-furtwangen.de <fxcbykz9655>
2019-12-03 16:15:48 +0100

    App vue aktualisieren Login_vue farben anpassen Home.vue aktualisieren

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
                    color="red"
                    dark
                    height="50">
-            <v-toolbar-title left>
-                <router-link to="/home" tag="span" style="cursor: pointer">Y-Subs</router-link>
-            </v-toolbar-title>
-            <v-spacer/>
+              <v-img
+                  class="mx-2"
+                  src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png"
+                  max-height="40"
+                  max-width="40"
+                  contain
+               ></v-img>
+               <v-toolbar-title left>
+                   <router-link to="/home" tag="span" style="cursor: pointer">Y-Subs</router-link>
+                </v-toolbar-title>
+                <v-spacer/>
 
               <v-toolbar-items>
                   <v-btn text router to="/login">
