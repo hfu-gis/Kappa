1c083366555304932699fb3b3551ad53cba42e1b
Janni344 <jan-luca.maximilian.haspel@hs-furtwangen.de>
2019-12-10 15:27:52 +0100

    Registrieren/Passwort Vergessen

--- src/views/abobearbeiten.vue
+++ src/views/abobearbeiten.vue
@@ -2,30 +2,31 @@
     <div>
         <fieldset>
             <h1>Abo bearbeiten <br></h1>
-            Anbieter<br>
+            Anbieter wählen: <br>
             <select name="cars">
-                <option value="netflix">Neflix</option>
+                <option value="netflix">Netflix</option>
                 <option value="amazon">Amazon</option>
                 <option value="disney+">Disney+</option>
                 <option value="Anderer">Anderer</option>
-            </select><br><br>
+            </select><br>
+            Eigenen Anbieter hinzufügen:<br>
             <input type="text">
             <br><br>
             Währung<br>
             <input type="radio" name="währung" value="male" checked> Euro<br>
             <input type="radio" name="währung" value="female"> Dollar<br>
             <input type="radio" name="währung" value="other"> Franken<br><br>
-            Startdatum<br>
+            Startdatum:<br>
             <input type="date" value="2018-09-17"><br><br>
-            Laufzeit<br>
+            Laufzeit:<br>
             <input type="date" value="2020-09-17"><br><br>
             Kosten<br>
             <input type="number" value="8" min="0" step="0.01"><br><br>
             Benutzer<br>
             <input type="number" value="1" name="bentzer" min="1" max="4"><br><br>
             Kündigungsfrist<br>
-            <input type="number" name="kündigungsfrist" value="4" min="0" step="1"> Wochen<br><br>
-            <input type="submit" value="Speichern">
+            <input type="number" name="kündigungsfrist" value="4" min="0" step="1"> Wochen<br><br><br>
+            <input type="submit" value="Speichern"> /
             <input type="reset" value="Abbrechen">
         </fieldset>
     </div>
