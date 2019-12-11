551c09137ae8092e6c4622ed10919e04448cc99c
RedouAin <56726424+RedouAin@users.noreply.github.com>
2019-12-11 12:06:57 +0100

    Datenbank db.js hinzugefügt

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAb3ukfQIosE--0V98t9pLpEIsfNDaACxE",
+    authDomain: "y-subs-78fcd.firebaseapp.com",
+    databaseURL: "https://y-subs-78fcd.firebaseio.com",
+    projectId: "y-subs-78fcd",
+    storageBucket: "y-subs-78fcd.appspot.com",
+    messagingSenderId: "741297548210",
+    appId: "1:741297548210:web:9993e1562ec26eaed752ab",
+    measurementId: "G-CJNWV2XHM6"
+};
