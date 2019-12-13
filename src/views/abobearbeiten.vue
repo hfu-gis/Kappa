2df7679684c759eea2a0a5000342f03895749faa
Janni344 <jan-luca.maximilian.haspel@hs-furtwangen.de>
2019-12-13 14:43:45 +0100

    Registrieren/Passwort Vergessen

--- src/views/abobearbeiten.vue
+++ src/views/abobearbeiten.vue
@@ -4,8 +4,8 @@
             <h1>Abo bearbeiten <br></h1>
             Anbieter wählen: <br>
             <select name="cars">
-                <v-system-bar></v-system-bar>
-                <option value="netflix">Netflix</option>
+
+                <v-system-bar></v-system-bar>  <option value="netflix">Netflix</option>
                 <option value="amazon">Amazon</option>
                 <option value="disney+">Disney+</option>
                 <option value="Anderer">Anderer</option>np
