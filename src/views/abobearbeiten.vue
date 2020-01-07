603964aa8c95f43e098dd69ce28dde89d3bcf0eb
Janni344 <jan-luca.maximilian.haspel@hs-furtwangen.de>
2020-01-07 16:57:50 +0100

    Registrieren/Passwort Vergessen

--- src/views/abobearbeiten.vue
+++ src/views/abobearbeiten.vue
@@ -1,38 +1,28 @@
-<br><br>
-<br><br>
 <template>
-    <div>
-        <fieldset>
-            <h1>Abo bearbeiten <br></h1>
-            Anbieter wählen: <br>
-            <select name="cars">
+    <v-card
+            max-width="10000"
+            outlined
+    >
+        <v-list-item three-line>
+            <v-list-item-content>
+                <div class="overline mb-4">OVERLINE</div>
+                <v-list-item-title class="headline mb-1">Abo Bearbeiten</v-list-item-title>
+                <v-list-item-subtitle>Anbieter wählen: Abo </v-list-item-subtitle>
+                <v-list-item-subtitle>Währung wählen: Euro</v-list-item-subtitle>
+                <v-list-item-subtitle>Startdatum: 01.01.2020</v-list-item-subtitle>
+                <v-list-item-subtitle>Laufzeit: 1 Jahr</v-list-item-subtitle>
+                <v-list-item-subtitle>Kosten: 10 Euro</v-list-item-subtitle>
 
-                <option value="netflix">Netflix</option>
-                <option value="amazon">Amazon</option>
-                <option value="disney+">Disney+</option>
-                <option value="Anderer">Anderer</option>np
-            </select><br>
-            Eigenen Anbieter hinzufügen:<br>
-            <input type="text">
-            <br><br>
-            Währung<br>
-            <input type="radio" name="währung" value="male" checked> Euro<br>
-            <input type="radio" name="währung" value="female"> Dollar<br>
-            <input type="radio" name="währung" value="other"> Franken<br><br>
-            Startdatum:<br>
-            <input type="date" value="2018-09-17"><br><br>
-            Laufzeit:<br>
-            <input type="date" value="2020-09-17"><br><br>
-            Kosten<br>
-            <input type="number" value="8" min="0" step="0.01"><br><br>
-            Benutzer<br>
-            <input type="number" value="1" name="bentzer" min="1" max="4"><br><br>
-            Kündigungsfrist<br>
-            <input type="number" name="kündigungsfrist" value="4" min="0" step="1"> Wochen<br><br><br>
-            <input type="submit" value="Speichern"> /
-            <input type="reset" value="Abbrechen">
-        </fieldset>
-    </div>
+            </v-list-item-content>
+
+
+        </v-list-item>
+
+        <v-card-actions>
+            <v-btn text>Speichern</v-btn>
+            <v-btn text>Abbrechen</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 
@@ -40,6 +30,7 @@
     export default {
         name: "abobearbeiten"
     }
+
 </script>
 
 <style scoped>
